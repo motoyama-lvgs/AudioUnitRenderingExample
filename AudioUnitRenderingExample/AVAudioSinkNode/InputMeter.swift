a6fc932389f594fdaf2563d2edef6dd4933c24ae
takahiro motoyama <takahiro.motoyama@leverages.jp>
2021-02-25 18:50:15 +0900

    コメント

--- AudioUnitRenderingExample/AVAudioSinkNode/InputMeter.swift
+++ AudioUnitRenderingExample/AVAudioSinkNode/InputMeter.swift
@@ -12,7 +12,7 @@ import AVFoundation
 
 class InputMeter {
     var value: InputMeterRendererValue { renderer.value }
-    
+    // subjectをpublisherに変換しているイメージ
     var lostPublisher: AnyPublisher<Void, Never> { lostSubject.eraseToAnyPublisher() }
     
     private let session: AudioSession
@@ -27,32 +27,39 @@ class InputMeter {
         case startEngine
     }
     
+    // default値を.sharedでつっこむ。型から類推してくれてるっぽい
     init(session: AudioSession = .shared) {
         self.session = session
     }
     
     deinit {
+        // 後始末
         stop()
     }
     
     func start() throws {
         do {
+            // sessionをactivate
             try session.activate(category: .record)
         } catch {
             throw StartError.setupAudioSession
         }
         
+        // 入力関連のhardware/channelが有効かcheck
         guard session.hasInput else {
             throw StartError.inputUnavailable
         }
         
+        // イヤホン/aipodsの切り替えの際に発火
         AudioSession.shared.rerouteSubject.sink { [weak self] in
             self?.reroute()
         }.store(in: &cancellables)
         
+        // 次のpublisherに渡しているだけ
         AudioSession.shared.lostSubject.subscribe(lostSubject).store(in: &cancellables)
         
         do {
+            // engin開始
             try setupAndStartEngine()
         } catch {
             throw StartError.startEngine
@@ -60,43 +67,52 @@ class InputMeter {
     }
     
     func stop() {
+        // engineを止める(消す)
         disposeEngine()
-        
+        // disposeの中身を全部消す
         cancellables.removeAll()
-        
+        // deactive
         session.deactivate()
     }
 }
 
 private extension InputMeter {
     func setupAndStartEngine() throws {
+        // avaudioenginのinstance作成
         let engine = AVAudioEngine()
-        
+        // 0から開始?(input)
         let inputFormat = engine.inputNode.inputFormat(forBus: 0)
+        // objective-c経由でsinknodeを作成
         let sinkNode = renderer.makeSineNode(sampleRate: inputFormat.sampleRate);
-        
+        // enginにattach
         engine.attach(sinkNode)
+        // engine内のinputnodeをsinknodeにつなげる
         engine.connect(engine.inputNode, to: sinkNode, format: nil)
-        
+        // 0から開始?(output)
         let outputFormat = engine.inputNode.outputFormat(forBus: 0)
         print("InputMeter Engine - format : \(outputFormat)")
         
+        // ここで初めてスタート
         try engine.start()
         
+        // instanceを保管しておく
         self.engine = engine
     }
     
     func disposeEngine() {
+        // 止めてからnilにする
         engine?.stop()
         engine = nil
     }
     
+    // hardwareに変更があったときに再起動するノリ
     func reroute() {
         disposeEngine()
         
         do {
             try setupAndStartEngine()
         } catch {
+            // エラーの場合はlostSubjectにsendする
             self.lostSubject.send()
         }
     }
--- AudioUnitRenderingExample/AVAudioSinkNode/InputMeterViewController.swift
+++ AudioUnitRenderingExample/AVAudioSinkNode/InputMeterViewController.swift
@@ -18,15 +18,18 @@ class InputMeterViewController: UIViewController {
     private let meter = InputMeter()
     private var displayLink: CADisplayLink?
     
+    // Set: Arrayと違って、①値はUniqueになり、②順序は担保されない
     private var cancellables = Set<AnyCancellable>()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         meter.lostPublisher.sink { [weak self] _ in
+            // lostした場合はalertが出る
             self?.showAlert(title: "Lost", message: nil)
         }.store(in: &cancellables)
         
+        // 多分音の可視化されている部分の動きをここの制約で制御している
         levelConstraint.constant = 0.0
         peakConstraint.constant = 0.0
     }
@@ -35,44 +38,51 @@ class InputMeterViewController: UIViewController {
         super.viewDidAppear(animated)
         
         do {
+            // 計測開始
             try meter.start()
         } catch {
             let message = (error as? InputMeter.StartError).flatMap { "\($0)" }
             showAlert(title: "Error", message: message)
         }
         
+        // CADisplayLink: 画面のリフレッシュレートと同期して描画させるタイマーオブジェクト
         displayLink = CADisplayLink(target: self, selector: #selector(updateMeter))
+        // 生成したCADisplayLinkのinstanceをmain loopに追加してあげている感じ
         displayLink?.add(to: .main, forMode: .default)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
+        // 後始末
         displayLink?.invalidate()
         displayLink = nil
-        
+        // 計測終了
         meter.stop()
     }
 }
 
 private extension InputMeterViewController {
     func showAlert(title: String, message: String?) {
+        // ありふれたalert
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alert.addAction(.init(title: "OK", style: .default) { [weak self] _ in self?.popViewController() })
         present(alert, animated: true, completion: nil)
     }
     
     func popViewController() {
+        // popToRootViewController: 1番最初のcontrollerに戻る
         navigationController?.popToRootViewController(animated: true)
     }
     
     @objc func updateMeter() {
         let value = meter.value
         
+        // barの横幅を元に、levelとpeakの長さを算出し制約に反映する
         let maxWidth = baseView.frame.width
         levelConstraint.constant = CGFloat(value.level) * maxWidth
         peakConstraint.constant = CGFloat(value.peak) * maxWidth
         
+        // 表示用にdecibelに変換
         let dbValue = String(format: "%.1f", decibelFromLinear(value.peak))
         meterLabel.text = "\(dbValue) dB"
     }
